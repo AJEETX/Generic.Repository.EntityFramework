4d1cd1e39438a90ab8c9801e61d154315c8f8e75
ajeet kumar <ajeetkumar@email.com>
2018-02-10 16:59:38 +1100

    renaming unit tests

---
+++ EntityFrameworkWrapper/EntityFrameworkWrapper.Test/RepositoryTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EntityFrameworkWrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkWrapper.Tests
+{
+    [TestClass()]
+    public class RepositoryTests
+    {
+        [TestMethod()]
+        public void RepositoryAddTest_adds_the_item_successfully()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RepositoryFindTest_finds_the_items_successfully()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RepositoryGetTest_gets_the_items_successfully()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RepositoryRemoveTest_removes_the_item_successfully()
+        {
+            Assert.Fail();
+        }
+    }
+}
