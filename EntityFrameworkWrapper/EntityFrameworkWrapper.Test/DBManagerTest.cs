eb567d30678643b946b1ace2e2651b25e0b8ebc9
AJEETX <ajeetkumar@lycos.com>
2018-02-10 18:59:49 +1100

    Update DBManagerTest.cs

--- EntityFrameworkWrapper/EntityFrameworkWrapper.Test/DBManagerTest.cs
+++ EntityFrameworkWrapper/EntityFrameworkWrapper.Test/DBManagerTest.cs
@@ -9,11 +9,34 @@ namespace EntityFrameworkWrapper.Test
         [TestMethod]
         public void TestMethod_GetRepository_returns_the_type_requested_repository()
         {
+            //Arrange
+            _IMockIDbcontext = new Mock<IDbContext>();
+            var transationManager = new DBManager(_IMockIDbcontext.Object);
+
+            //Act
+            var result = transationManager.CreateRepository<ClientData>();
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(IRepository<ClientData>));
+            Assert.IsNotNull(result);
         }
         [TestMethod]
         public void TestMethod_Save_returns_point_integer_on_succesfull_operation()
         {
+            //Arrange
+            _IMockIDbcontext = new Mock<IDbContext>();
+            _IMockIDbcontext.Setup(m => m.SaveChange()).Returns(1);
+            var transationManager = new DBManager(_IMockIDbcontext.Object);
 
+            //Act
+            var result = transationManager.Save();
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(int));
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result, 1);
+            _IMockIDbcontext.Verify(v => v.SaveChange(), Times.Once);
+        
         }
     }
 }
