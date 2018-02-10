9471251f85362d09f2621699bbf53dcd8365ea6c
ajeet kumar <ajeetkumar@email.com>
2018-02-10 16:29:07 +1100

    Added IDbcontext interface

---
+++ EntityFrameworkWrapper/EntityFrameworkWrapper.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EntityFrameworkWrapper.Test
+{
+    [TestClass]
+    public class UnitTestIDbContext
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+        }
+    }
+}
