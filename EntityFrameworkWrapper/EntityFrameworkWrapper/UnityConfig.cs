305f26b879c5ce0511b7e17159ad56f31e43ebc2
Ajeet <ajeet.tiwari@helloworld.com.au>
2018-02-20 12:32:15 +1100

    Access to Db handler Interface

---
+++ EntityFrameworkWrapper/EntityFrameworkWrapper/UnityConfig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace EntityFrameworkWrapper
+{
+    public class UnityConfig
+    {
+        public static IDBManager GetDBManager()
+        {
+            IUnityContainer unitycontainer = new UnityContainer();
+            unitycontainer.RegisterType<IDBManager, DBManager>();
+            unitycontainer.RegisterType(typeof(IRepository<>),typeof(Repository<>));
+            return unitycontainer.Resolve<IDBManager>();
+        }
+    }
+}
